fec4424f871d55ee9ea4b386b7821cdecd71de1a
vaykz <gabrielrasmuniz@gmail.com>
2024-10-17 20:56:56 -0300

    add board setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+import MarsBoard from './components/MarsBoard.vue'
 </script>
 
 <template>
-  <main>
-    <img alt="mars-board" src="./shared/images/mars.jpg" width="125" height="125" />
-  </main>
+  <div class="container">
+    <MarsBoard />
+  </div>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 </style>
---
+++ frontend/src/components/MarsBoard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="board">
+    <div class="grid">
+      <div v-for="n in 225" :key="n" class="grid-item"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.board {
+  position: relative;
+  width: 800px;
+  height: 800px;
+  background-image: url('../shared/images/mars.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+  grid-template-rows: repeat(auto-fill, minmax(50px, 1fr));
+  gap: 1px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.grid-item {
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
